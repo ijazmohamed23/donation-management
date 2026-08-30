dda0146a4c42a2ec2f9d065d739abd3984a29af9
Ijaz Mohamed <153897329+ijazmohamed23@users.noreply.github.com>
2026-08-30 19:14:34 +0530

    fix(security): autonomous fix by GateKeeper AI for config/sampletest.py

--- config/sampletest.py
+++ config/sampletest.py
@@ -2,7 +2,7 @@
 import os
 
 # Hardcoded Cloud Secret (GateKeeper AI will detect this!)
-AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_ACCESS_KEY_ID = os.getenv("AKIAIOSFODNN7EXAMPLE_ENV_VAR", "")
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
 def connect_s3():
